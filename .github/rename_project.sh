d40b352bee2f12c38d2cda22319759a59b256760
jianliao <jianliao@users.noreply.github.com>
2024-04-05 01:42:06 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="jianliao"
+original_name="newton_raphson_zeros"
+original_urlname="newton-raphson-zeros"
+original_description="Awesome newton_raphson_zeros created by jianliao"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv newton_raphson_zeros $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
---
+++ newton_raphson_zeros/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for newton_raphson_zeros."""
+
+from newton_raphson_zeros.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
